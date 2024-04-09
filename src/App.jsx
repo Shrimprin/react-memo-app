6010cec56071ae1b39870c837c2ca0bb97c96292
Kuroneko <ken.aster8@gmail.com>
2024-04-09 20:58:23 +0900

    localstorageを使ってメモを永続化

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Form from "./components/Form";
 import List from "./components/List";
 import { v4 as uuid } from "uuid";
 
 export default function App() {
-  const [memos, setMemos] = useState([]);
+  const [memos, setMemos] = useState(
+    JSON.parse(localStorage.getItem("memos")) || []
+  );
   const [isFormActive, setIsFormActive] = useState(false);
 
+  useEffect(() => {
+    localStorage.setItem("memos", JSON.stringify(memos));
+  }, [memos]);
+
   const addMemo = (content) => {
-    setMemos([...memos, { id: uuid(), content: content }]);
+    const newMemo = { id: uuid(), content: content };
+    const updatedMemos = [...memos, newMemo];
+    setMemos(updatedMemos);
   };
 
   const deleteMemo = (id) => {
